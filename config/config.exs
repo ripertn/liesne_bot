ec62d37fb44dd0015a82a1b249bd72c4e71d9117
abraham <nathanripert77@gmail.com>
2022-11-21 11:37:47 +0100

    add version number...

--- config/config.exs
+++ config/config.exs
@@ -3,5 +3,5 @@ import Config
 config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
 
 # if config_env() == :dev do
-  config :liesne_bot, :version, "v0_0_1"
+  config :liesne_bot, :version, "v0_0_2"
 # end
